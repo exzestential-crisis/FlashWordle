c120f4418030cba9364fe36e738b7115208689c5
exze-alegre <akina.alegre@gmail.com>
2025-09-27 13:44:32 +0800

    Eslint ignore

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // <-- disables ESLint during build
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
